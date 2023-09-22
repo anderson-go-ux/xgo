0f7510b81dc0d3132b0cc549bbe06ca2feb2762f
abu <abu@gmail.com>
2023-09-23 04:06:26 +0800

    'auto'

--- commit.bat
+++ commit.bat
@@ -2,9 +2,7 @@
 git add *
 git commit -m'auto'
 git push
-IF "%1"=="" (
-    echo No tag name provided. Exiting...
-) ELSE (
-    git tag %1
+IF not  "%1"=="" (
+     git tag %1
     git push --tags
 )
