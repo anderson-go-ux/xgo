d24bf74e6dcd427568113fbb4c161cea3dadfd6f
abu <abu@gmail.com>
2023-09-24 16:39:15 +0800

    'auto'

--- commit.bat
+++ commit.bat
@@ -1,19 +1,11 @@
 @echo off
-setlocal enabledelayedexpansion
-set "param=%~1"
-:trimLeading
-if "!param:~0,1!"==" " set "param=!param:~1!" & goto :trimLeading
-:trimTrailing
-if "!param:~-1!"==" " set "param=!param:~0,-1!" & goto :trimTrailing
-
-IF not !param!=="" (
-   echo > version.txt !param!
+IF not  "%1"=="" (
+   echo  %1 > version.txt
 )
 git add *
 git commit -m'auto'
 git push
-IF not !param!=="" (
+IF not  "%1"=="" (
     git tag %1
     git push --tags
 )
-endlocal
