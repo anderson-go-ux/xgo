b34f2172cc483fc3f2c64624a2a9153b2b654318
abu <917628331@qq.com>
2023-11-03 13:29:23 +0800

    auto commit

--- main.go
+++ main.go
@@ -16,21 +16,5 @@ func main() {
 	http.InitWs("/sapi/ws")
 	http.InitWs("/capi/ws")
 	xgo.AdminInit(http, db, redis)
-	xgo.ShowTable(db, "x_config")
 	xgo.Run()
-}
-
-type XConfig struct {
-	Id          int    `gorm:"column:Id" json:"Id"`                   //
-	SellerId    int    `gorm:"column:SellerId" json:"SellerId"`       // 运营商
-	ChannelId   int    `gorm:"column:ChannelId" json:"ChannelId"`     // 渠道
-	ConfigName  string `gorm:"column:ConfigName" json:"ConfigName"`   // 配置名
-	ConfigValue string `gorm:"column:ConfigValue" json:"ConfigValue"` // 配置值
-	ForClient   int    `gorm:"column:ForClient" json:"ForClient"`     // 该配置客户端是否能获取
-	Memo        string `gorm:"column:Memo" json:"Memo"`               // 备注
-	CreateTime  string `gorm:"column:CreateTime" json:"CreateTime"`   // 创建时间
-}
-
-func (this *XConfig) TableName() string {
-	return "x_config"
 }
